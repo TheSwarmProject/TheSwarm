512843a2d3b8d303fff04581ba63a1d0e46d63b0
Vizan Arkonin <altahir911@gmail.com>
2023-08-14 17:42:41 +0300

    ~ Changed RegisterSwarmClient processing - i didn't account for a fact that user may be using multiple clients at once. Now it will process all occurrences of the annotation

--- Core/Interfaces/TaskSet.cs
+++ Core/Interfaces/TaskSet.cs
@@ -7,44 +7,52 @@ namespace TheSwarmClient.Interfaces;
 /// <summary>
 /// Internal wrapper, used to initialize and process the SwarmTaskSet type.
 /// </summary>
-internal class TaskSet {
-    private object taskSetInstance {get; init;}
-    private SwarmTaskSet taskSetParams {get; set;}
-    protected TaskExecutor Executor {get; set;}
-    private Random rnd {get; init;} = new Random();
-    public SwarmTaskSetSetup? Setup {get; init;}
-    public SwarmTaskSetTeardown? Teardown {get; init;}
-    public SwarmBeforeTask? BeforeTask {get; init;}
-    public SwarmAfterTask? AfterTask {get; init;}
-    private List<SwarmTask> Tasks {get; init;} = new List<SwarmTask>();
-    private int totalTasksWeight {get; init;} = 0;
+internal class TaskSet
+{
+    private object taskSetInstance          { get; init; }
+    private SwarmTaskSet taskSetParams      { get; set; }
+    protected TaskExecutor Executor         { get; set; }
+    private Random rnd                      { get; init; } = new Random();
+    public SwarmTaskSetSetup? Setup         { get; init; }
+    public SwarmTaskSetTeardown? Teardown   { get; init; }
+    public SwarmBeforeTask? BeforeTask      { get; init; }
+    public SwarmAfterTask? AfterTask        { get; init; }
+    private List<SwarmTask> Tasks           { get; init; } = new List<SwarmTask>();
+    private int totalTasksWeight            { get; init; } = 0;
 
-    public TaskSet(TaskExecutor executor) {
+    public TaskSet(TaskExecutor executor)
+    {
         this.Executor = executor;
-        if (executor.TaskSet.GetCustomAttribute(typeof(SwarmTaskSet)) is not null) {
+        if (executor.TaskSet.GetCustomAttribute(typeof(SwarmTaskSet)) is not null)
+        {
             this.taskSetInstance = Activator.CreateInstance(executor.TaskSet);
-            this.taskSetParams = (SwarmTaskSet) taskSetInstance.GetType().GetCustomAttribute(typeof(SwarmTaskSet));
-        } else
+            this.taskSetParams = (SwarmTaskSet)taskSetInstance.GetType().GetCustomAttribute(typeof(SwarmTaskSet));
+        }
+        else
             throw new Exception($"{executor.TaskSet.ToString()} is not marked as SwarmTaskSet. Make sure it has SwarmTaskSet annotation added and try again");
 
         MethodInfo? m = this.taskSetInstance.GetType().GetMethods().Where(m => m.GetCustomAttributes(typeof(SwarmTaskSetSetup), false).Length > 0).FirstOrDefault();
-        if (m is not null) {
-            this.Setup = ((SwarmTaskSetSetup) m.GetCustomAttribute(typeof(SwarmTaskSetSetup)));
+        if (m is not null)
+        {
+            this.Setup = ((SwarmTaskSetSetup)m.GetCustomAttribute(typeof(SwarmTaskSetSetup)));
             this.Setup.SetMethod((Action)Delegate.CreateDelegate(typeof(Action), this.taskSetInstance, m));
         }
         m = this.taskSetInstance.GetType().GetMethods().Where(m => m.GetCustomAttributes(typeof(SwarmTaskSetTeardown), false).Length > 0).FirstOrDefault();
-        if (m is not null) {
-            this.Teardown = ((SwarmTaskSetTeardown) m.GetCustomAttribute(typeof(SwarmTaskSetTeardown)));
+        if (m is not null)
+        {
+            this.Teardown = ((SwarmTaskSetTeardown)m.GetCustomAttribute(typeof(SwarmTaskSetTeardown)));
             this.Teardown.SetMethod((Action)Delegate.CreateDelegate(typeof(Action), this.taskSetInstance, m));
         }
         m = this.taskSetInstance.GetType().GetMethods().Where(m => m.GetCustomAttributes(typeof(SwarmBeforeTask), false).Length > 0).FirstOrDefault();
-        if (m is not null) {
-            this.BeforeTask = ((SwarmBeforeTask) m.GetCustomAttribute(typeof(SwarmBeforeTask)));
+        if (m is not null)
+        {
+            this.BeforeTask = ((SwarmBeforeTask)m.GetCustomAttribute(typeof(SwarmBeforeTask)));
             this.BeforeTask.SetMethod((Action)Delegate.CreateDelegate(typeof(Action), this.taskSetInstance, m));
         }
         m = this.taskSetInstance.GetType().GetMethods().Where(m => m.GetCustomAttributes(typeof(SwarmAfterTask), false).Length > 0).FirstOrDefault();
-        if (m is not null) {
-            this.AfterTask = ((SwarmAfterTask) m.GetCustomAttribute(typeof(SwarmAfterTask)));
+        if (m is not null)
+        {
+            this.AfterTask = ((SwarmAfterTask)m.GetCustomAttribute(typeof(SwarmAfterTask)));
             this.AfterTask.SetMethod((Action)Delegate.CreateDelegate(typeof(Action), this.taskSetInstance, m));
         }
 
@@ -52,24 +60,29 @@ internal class TaskSet {
 
         if (tasks.Count == 0)
             throw new Exception("No tasks were defined in taskset. Use SwarmTask annotation to mark a public void method as a task");
-        foreach(MethodInfo method in tasks) 
-            if(!method.IsStatic) {
-                SwarmTask task = (SwarmTask) method.GetCustomAttribute(typeof(SwarmTask));
+        foreach (MethodInfo method in tasks)
+            if (!method.IsStatic)
+            {
+                SwarmTask task = (SwarmTask)method.GetCustomAttribute(typeof(SwarmTask));
                 task.SetMethod((Action)Delegate.CreateDelegate(typeof(Action), this.taskSetInstance, method));
                 this.Tasks.Add(task);
                 if (task.Weight != 0)
                     totalTasksWeight += task.Weight;
             }
-        
+
         // Since taskset is created as object during runtime, we look for RegisterSwarmClient in runtime properties.
-        PropertyInfo? prop = this.taskSetInstance.GetType().GetRuntimeProperties()
+        List<PropertyInfo> clientProps = this.taskSetInstance.GetType().GetRuntimeProperties()
             .Where(p => p.GetCustomAttributes(typeof(RegisterSwarmClient), false).Length > 0)
-            .FirstOrDefault();
-        if(prop is not null)
-            if (prop.GetValue(this.taskSetInstance).GetType().IsSubclassOf(typeof(SwarmClient)))
-                ((SwarmClient) prop.GetValue(this.taskSetInstance)).SetTaskExecutor(executor);
-            else
-                throw new Exception($"Type {prop.GetType().Name} is not a sub-class of SwarmClient and thus cannot be registered as one");
+            .ToList();
+        // Given the fact that user can use multiple clients - we process all occurrences of the attribute
+        foreach (PropertyInfo prop in clientProps)
+        {
+            if (prop is not null)
+                if (prop.GetValue(this.taskSetInstance).GetType().IsSubclassOf(typeof(SwarmClient)))
+                    ((SwarmClient)prop.GetValue(this.taskSetInstance)).SetTaskExecutor(executor);
+                else
+                    throw new Exception($"Type {prop.GetType().Name} is not a sub-class of SwarmClient and thus cannot be registered as one");
+        }
     }
 
     /// <summary>
@@ -77,7 +90,8 @@ internal class TaskSet {
     /// NOTE: If task weights were set - it will pick it using PickRandomTaskWithWeight method (i.e. using weight as factor). If not - it will pick one at random.
     /// </summary>
     /// <returns>SwarmTask to execute</returns>
-    public SwarmTask PickRandomTask() {
+    public SwarmTask PickRandomTask()
+    {
         if (totalTasksWeight > 0)
             return PickRandomTaskWithWeight();
         else
@@ -91,12 +105,15 @@ internal class TaskSet {
     /// If no tasks were picked by an algorithm - a random task will be picked.
     /// </summary>
     /// <returns>SwarmTask to execute</returns>
-    private SwarmTask PickRandomTaskWithWeight() {
+    private SwarmTask PickRandomTaskWithWeight()
+    {
         int randomInt = rnd.Next(0, totalTasksWeight);
 
         SwarmTask task = null;
-        foreach (SwarmTask swarmTask in Tasks){
-            if (randomInt < swarmTask.Weight){
+        foreach (SwarmTask swarmTask in Tasks)
+        {
+            if (randomInt < swarmTask.Weight)
+            {
                 task = swarmTask;
                 break;
             }
